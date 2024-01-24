b1023265973a553d8e4d29ef6fd2160f4f4293f6
Sevasit <new12253952@gmail.com>
2024-01-24 20:16:25 +0700

    first commit

---
+++ app/src/main/java/com/example/myappsevasitsecc/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.myappsevasitsecc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+    private static final long SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
